253fe272211ec7a1165cc560b1ff6d33f70bbcfe
Lee.li <Lim.123456>
2019-05-29 18:18:33 +0800

    ps:import调整

--- multi_processframe/TestCase/__init__.py
+++ multi_processframe/TestCase/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = "Lee.li"
-# import os
-# from Tools import scriptList
-# route = os.getcwd()
-# script_List = scriptList.get_script_list(route)
-# __all__ = script_List
+import os
+from multi_processframe.Tools import analysis
+
+case_Path = os.path.join(os.getcwd(), "TestCase")
+script_List = analysis.get_script_list(case_Path)
+__all__ = script_List
--- multi_processframe/__init__.py
+++ multi_processframe/__init__.py
@@ -1,5 +1,5 @@
 # -*- encoding=utf8 -*-
 #构造方法
 
-# __all__ = ["Tools","TestCase"]
-# print(__all__)
+__all__ = ["Tools","TestCase"]
+print(__all__)
