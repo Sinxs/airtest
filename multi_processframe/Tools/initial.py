7047d3da9cd339a67983d38405d4201de5318115
Sinwu <157366763@QQ.com>
2019-05-29 20:30:19 +0800

    ps：提交优化脚本重置优环境代码

--- multi_processframe/Tools/initial.py
+++ multi_processframe/Tools/initial.py
@@ -6,16 +6,17 @@ from multi_processframe.Tools import get_screen_size
 
 def startgame(devices):
     dev = connect_device("android:///" + devices)
+    sleep(3)
     poco = UnityPoco(device=dev)
     if os.system(f"adb -s {devices} shell pidof com.playfungame.ggplay.lzgsea") == 1:
         print("游戏未启动，开始启动游戏...")
         stop_app('com.playfungame.ggplay.lzgsea')
         sleep(1)
         start_app('com.playfungame.ggplay.lzgsea')
-        sleep(35)
+        sleep(40)
         poco = UnityPoco(device=dev)
         touch([int(get_screen_size._get_screen_size(devices)[0]*0.5),int(get_screen_size._get_screen_size(devices)[1]*0.5)])
-        sleep(20)
+        sleep(25)
         if poco(text="进入游戏").exists():
             poco(text="进入游戏").click()
             print("点击进入游戏，开始选择角色。。。")
@@ -30,12 +31,14 @@ def startgame(devices):
             print("进入游戏失败，请检查。。。。")
     else:
         print("游戏已经启动，无需再次启动...")
-    for x in range(10):
-        l_close = poco(texture="l_close_00")
-        Close = poco("Close")
+    l_close = poco(texture="l_close_00")
+    Close = poco("Close")
+    for x in range(4):
         if l_close.exists() and Close.exists():
+            sleep(1.5)
             poco(texture="l_close_00").click()
         elif Close.exists():
+            sleep(1.5)
             Close.click()
         else:
             break
