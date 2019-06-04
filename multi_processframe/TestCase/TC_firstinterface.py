788131d9a12b0c49d3c0985921a0a06107a50bf1
xiaomingli <xiaomingli@123u.com>
2019-06-04 15:39:48 +0800

    修改命名

--- multi_processframe/TestCase/TC_firstinterface.py
+++ multi_processframe/TestCase/TC_firstinterface.py
@@ -5,7 +5,7 @@ import unittest
 from airtest.core.api import *
 from Script.mainectype import main_ectype
 from multi_processframe.Tools import initial, screenshot
-from Script.firstinterface import Firstinterface
+from Script.index import Firstinterface
 
 
 def Main(devices):
