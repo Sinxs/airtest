2c43789d6005e0707f1ae436108ecafd13eccf61
sinwu <157366763@QQ.com>
2019-06-12 14:36:27 +0800

    ps：提交图鉴模块代码

---
+++ Script/smoking/card.py
@@ -0,0 +1,28 @@
+from multi_processframe.Tools import printcolor,adb_connect
+from airtest.core.api import *
+from poco.drivers.unity3d import UnityPoco
+
+devices = "127.0.0.1:62025"
+
+
+
+def card(devices):
+    poco = adb_connect.device(devices)
+    if poco("SysD_CardCollect").get_position()[1]>1:
+        poco(texture="halln_4").click()
+        poco("SysD_CardCollect").click()
+    else:
+        printcolor.printgreen("界面上就有图鉴按钮")
+        poco("SysD_CardCollect").click()
+    if poco("DeckPanel").exists():
+        for item in range(len(poco("DeckPanel").child())):
+            item1 = "item"+str(item)
+            poco("CardCollectDlg(Clone)").offspring("Deck").offspring("DeckPanel").child(item1).click()
+            if poco("CardCollectDlg(Clone)").offspring("Deck").offspring("DeckPanel").child(item1).offspring("Name").get_text() == poco("Title").get_text():
+                printcolor.printgreen("图鉴-->>  "+poco("Title").get_text()+"  界面显示正确")
+    else:
+        printcolor.printred("没有进入图鉴界面，请检查...")
+    return poco("Title").get_text()
+
+
+
---
+++ multi_processframe/TestCase/TC_card.py
@@ -0,0 +1,44 @@
+
+# -*- coding: utf-8 -*-
+__author__ = "Lee.li"
+
+import unittest
+from airtest.core.api import *
+from Script.smoking import card
+from multi_processframe.Tools import initial, screenshot
+
+def Main(devices):
+    class TC_card(unittest.TestCase):
+        u'''测试用例102的集合'''
+
+        @classmethod
+        def setUpClass(self):
+            u''' 这里放需要在所有用例执行前执行的部分'''
+            pass
+
+        def setUp(self):
+            u'''这里放需要在每条用例前执行的部分'''
+            initial.startgame(devices)
+
+        def test_guild(self):
+            """
+            图鉴-冒烟测试
+            """
+            try:
+                print("开始测试图鉴模块")
+                self.assertEqual("[b]莲花沼泽", card.card(devices))
+            finally:
+                screenshot.get_screen_shot(time.time(), devices, "交易所-冒烟测试")
+
+
+        def tearDown(self):
+            u'''这里放需要在每条用例后执行的部分'''
+            print(f"{devices}结束运行")
+
+        @classmethod
+        def tearDownClass(self):
+            u'''这里放需要在所有用例后执行的部分'''
+            pass
+
+    srcSuite = unittest.makeSuite(TC_card)
+    return srcSuite
