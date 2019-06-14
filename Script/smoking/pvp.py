b6e616a9616dd3b7e4cb73e66663ff75bb549f1d
xiaomingli <xiaomingli@123u.com>
2019-06-14 17:48:31 +0800

    ps:提交调试通过的竞技模块

--- Script/smoking/pvp.py
+++ Script/smoking/pvp.py
@@ -27,7 +27,7 @@ def athletics(devices):
                  printcolor.printgreen("竞技军衔，竞技名人堂，竞技副本显示正常")
              else:
                  printcolor.printred("竞技界面缺少元素，请详细查看")
-                 # screenshot.get_screen_shot(time.time(), devices, "竞技界面缺少元素，请详细查看")
+                 screenshot.get_screen_shot(time.time(), devices, "竞技界面缺少元素，请详细查看")
              name = freezepoco("PVPActivityFrame").offspring("Panel").child()
              print(f"PVP开放内容的描述为：{len(name)}项内容")
              count = 1
@@ -61,7 +61,7 @@ def athletics(devices):
                      printcolor.printgreen("规则 赛季时间，军衔奖励，挑战记录，排名，军衔，角色，军功元素显示正常")
                  else:
                      printcolor.printred("规则 赛季时间，军衔奖励，挑战记录，排名，军衔，角色，军功元素缺少，请具体核对")
-                     # screenshot.get_screen_shot(time.time(), devices, "竞技军衔按钮不存在")
+                     screenshot.get_screen_shot(time.time(), devices, "竞技军衔按钮不存在")
              poco("RewardBtn").click()
              poco("ToggleResult").click()
              with poco.freeze() as freezepoco:
@@ -88,12 +88,12 @@ def athletics(devices):
                      printcolor.printgreen("赛季奖励的条目正常")
                  else:
                      printcolor.printred("赛季奖励的条目不是8条，请前往查看少了哪个")
-                     # screenshot.get_screen_shot(time.time(), devices, "竞技军衔按钮不存在")
+                     screenshot.get_screen_shot(time.time(), devices, "竞技军衔按钮不存在")
              touch((140,389))
              poco("Close").click()
          else:
              printcolor.printred("竞技军衔按钮不存在")
-             # screenshot.get_screen_shot(time.time(), devices, "竞技军衔按钮不存在")
+             screenshot.get_screen_shot(time.time(), devices, "竞技军衔按钮不存在")
          if poco("Btn_MobaFamous").exists():
              poco("Btn_MobaFamous").click()
              with poco.freeze() as freezepoco:
@@ -107,7 +107,7 @@ def athletics(devices):
                      printcolor.printgreen("1:1天梯赛，英雄战场，全球竞技，跨服巅峰对决，点赞，规则，排行榜按钮显示正常")
                  else:
                      printcolor.printred("1:1天梯赛，英雄战场，全球竞技，跨服巅峰对决，点赞，规则，排行榜按钮显示异常")
-                     # screenshot.get_screen_shot(time.time(), devices, "天梯赛报错异常")
+                     screenshot.get_screen_shot(time.time(), devices, "天梯赛报错异常")
              if poco("HallFameDlg(Clone)").offspring("Help").exists():
                 poco("HallFameDlg(Clone)").offspring("Help").click()
                 if poco("ScrollView").exists():
@@ -151,29 +151,30 @@ def athletics(devices):
                         printcolor.printgreen("1:1天梯赛，评分奖励，排名奖励，荣誉商店，排行榜，对战记录，积分排名，开始匹配，技能教学，技能推荐按钮显示正常")
                     else:
                         printcolor.printred("1:1天梯赛，评分奖励，排名奖励，荣誉商店，排行榜，对战记录，积分排名，开始匹配，技能教学，技能推荐按钮显示异常")
-                        # screenshot.get_screen_shot(time.time(), devices, "天梯赛报错")
-                        try:
-                            poco("PointRewardBtn").click()
-                            poco(texture="l_close_00").click()
-                            poco("RankRewardBtn").click()
-                            poco(texture="l_close_00").click()
-                            poco("QualifierDlg(Clone)").child("Bg").offspring("ShopBtn").child("p").click()
-                            poco("Close").click()
-                            poco("RankBtn").click()
-                            poco(texture="l_close_00").click()
-                            poco("BattleRecordBtn").click()
-                            poco(texture="l_close_00").click()
-                            poco("P1").click()
-                            poco("TabTpl0").offspring("Selected").click()
-                            poco("Close").click()
-                            poco("Match1V1Btn").click()
-                            poco("Match1V1Btn").click()
-                            poco("Close").click()
-                            printcolor.printgreen("天梯赛所有按钮点击关闭操作正常")
-                        except Exception as e:
-                            printcolor.printred("天梯赛界面元素点击报错")
-                            printcolor.printred(e)
-                            # screenshot.get_screen_shot(time.time(), devices, "天梯赛界面元素点击报错")
+                        screenshot.get_screen_shot(time.time(), devices, "天梯赛报错")
+                    try:
+                        poco("PointRewardBtn").click()
+                        poco(texture="l_close_00").click()
+                        poco("RankRewardBtn").click()
+                        poco(texture="l_close_00").click()
+                        poco("QualifierDlg(Clone)").child("Bg").offspring("ShopBtn").child("p").click()
+                        poco("Close").click()
+                        poco("RankBtn").click()
+                        poco(texture="l_close_00").click()
+                        poco("BattleRecordBtn").click()
+                        poco(texture="l_close_00").click()
+                        poco("P1").click()
+                        poco("TabTpl0").offspring("Selected").click()
+                        poco("Close").click()
+                        poco("Match1V1Btn").click()
+                        poco("Match1V1Btn").click()
+                        poco("Close").click()
+                        printcolor.printgreen("天梯赛所有按钮点击关闭操作正常")
+                    except Exception as e:
+                        printcolor.printred("天梯赛界面元素点击报错")
+                        printcolor.printred(e)
+                        poco("Close").click()
+                        screenshot.get_screen_shot(time.time(), devices, "天梯赛界面元素点击报错")
                 poco("MobaActivityDlg(Clone)").offspring("item1").click()  # 进入角斗场
                 with poco.freeze() as freezepoco:
                     if freezepoco("Help").exists() and \
@@ -182,45 +183,47 @@ def athletics(devices):
                             freezepoco("ShopBtn").exists() and \
                             freezepoco("RecommededTip").exists() and \
                             freezepoco("MatchBtn").exists() and \
-                            freezepoco("AbattoirDlg(Clone)").offspring("Reward").child("item0").exists() and \
-                            freezepoco("AbattoirDlg(Clone)").offspring("Reward").child("item1").exists() and \
-                            freezepoco("AbattoirDlg(Clone)").offspring("Reward").child("item2").exists() and \
                             freezepoco("AbattoirDlg(Clone)").offspring("WinOfPoint").child("Num"):
                         printcolor.printgreen("三个排名奖励，升段奖励，排行榜，荣誉商店，参与宝箱，胜利宝箱，技能推荐按钮显示正常")
                     else:
                         printcolor.printred("三个排名奖励，升段奖励，排行榜，荣誉商店，参与宝箱，胜利宝箱，技能推荐按钮显示异常")
-                        # screenshot.get_screen_shot(time.time(), devices, "角斗场报错")
-                        try:
-                            poco("Help").click()
-                            poco("Btn").click()
-                            poco("AbattoirDlg(Clone)").offspring("PointRewardBtn").child("p").click()
-                            poco(texture="l_close_00").click()
-                            poco("RankBtn").click()
-                            poco(texture="l_close_00").click()
-                            poco("ShopBtn").click()
-                            poco("Close").click()
-                            poco("RecommededTip").click()
-                            poco("TabTpl0").offspring("Selected").click()
-                            poco("Close").click()
-                            poco("GoldJoyBtn").click()
+                        screenshot.get_screen_shot(time.time(), devices, "角斗场报错")
+                    try:
+                        poco("Help").click()
+                        poco("Btn").click()
+                        poco("AbattoirDlg(Clone)").offspring("PointRewardBtn").child("p").click()
+                        poco(texture="l_close_00").click()
+                        poco("RankBtn").click()
+                        poco(texture="l_close_00").click()
+                        poco("ShopBtn").click()
+                        poco("Close").click()
+                        poco("RecommededTip").click()
+                        poco("TabTpl0").offspring("Selected").click()
+                        poco("Close").click()
+                        poco("GoldJoyBtn").click()
+                        if poco("GoldBoxBtn").exists():
                             poco("GoldBoxBtn").click()
-                            printcolor.printgreen("角斗场所有按钮点击关闭操作正常")
-                            poco("MatchBtn").click()  # 进入角度场
-                            time.sleep(10)
-                            if poco("Pause").exists():
-                                time.sleep(5)
-                                poco("Pause").click()
-                                poco("Leave").click()
-                                time.sleep(15)
+                        printcolor.printgreen("角斗场所有按钮点击关闭操作正常")
+                        poco("MatchBtn").click()  # 进入角度场
+                        time.sleep(10)
+                        if poco("Pause").exists():
+                            time.sleep(5)
+                            poco("Pause").click()
+                            poco("Leave").click()
+                            time.sleep(15)
+                            poco("SysEPVP").click()
+                            printcolor.printgreen("角斗场进入退出正常")
+                            if poco("SysEPVP").exists():
                                 poco("SysEPVP").click()
-                                printcolor.printgreen("角斗场进入退出正常")
-                            else:
-                                printcolor.printred("角斗场进入失败")
-                                # screenshot.get_screen_shot(time.time(), devices, "角斗场进入失败")
-                        except Exception as e:
-                            printcolor.printred("角斗场界面元素点击报错")
-                            printcolor.printred(e)
-                            # screenshot.get_screen_shot(time.time(), devices, "角斗场界面元素点击报错")
+                        else:
+                            printcolor.printred("角斗场进入失败")
+                            poco("Close").click()
+                            screenshot.get_screen_shot(time.time(), devices, "角斗场进入失败")
+                    except Exception as e:
+                        printcolor.printred("角斗场界面元素点击报错")
+                        printcolor.printred(e)
+                        poco("Close").click()
+                        screenshot.get_screen_shot(time.time(), devices, "角斗场界面元素点击报错")
                 poco("MobaActivityDlg(Clone)").offspring("item2").click()  # 进入英雄战场
                 with poco.freeze() as freezepoco:
                     if freezepoco("Help").exists() and \
@@ -240,27 +243,28 @@ def athletics(devices):
                         printcolor.printgreen("规则，玩法攻略，技能预览，排行榜，商店，挑战记录，奖励预览，本周奖励，每日首胜，单人匹配，组队匹配，购买按钮，英雄列表等元素显示正常")
                     else:
                         printcolor.printred("规则，玩法攻略，技能预览，排行榜，商店，挑战记录，奖励预览，本周奖励，每日首胜，单人匹配，组队匹配，购买按钮，英雄列表等元素显示异常")
-                        # screenshot.get_screen_shot(time.time(), devices, "英雄战场点击报错")
-                        try:
-                            poco("Help").click()
-                            poco("Btn").click()
-                            poco("RankBtn").click()
-                            poco(texture="l_close_00").click()
-                            poco("HeroBattleDlg(Clone)").offspring("ShopBtn").child("P")[1].click()
-                            poco("Close").click()
-                            poco("RecordBtn")
-                            poco(texture="l_close_00").click()
-                            poco("RewardPreViewBtn").click()
-                            poco(texture="l_close_00").click()
-                            poco("SingleMatchBtn").click()
-                            poco("SingleMatchBtn").click()
-                            poco("TeamMatchBtn").click()
-                            poco("Close").click()
-                            printcolor.printgreen("英雄战场所有按钮点击关闭操作正常")
-                        except Exception as e:
-                            printcolor.printred("英雄战场界面元素点击报错")
-                            printcolor.printred(e)
-                            # screenshot.get_screen_shot(time.time(), devices, "英雄战场界面元素点击报错")
+                        screenshot.get_screen_shot(time.time(), devices, "英雄战场点击报错")
+                    try:
+                        poco("Help").click()
+                        poco("Btn").click()
+                        poco("RankBtn").click()
+                        poco("RankFrame").offspring("Bg").child("Close").click()
+                        poco("HeroBattleDlg(Clone)").offspring("ShopBtn").click()
+                        poco("MallDlg(Clone)").offspring("Bg").child("Close").click()
+                        poco("RecordBtn").click()
+                        poco("BattleRecordFrame").child("Close").click()
+                        poco("RewardPreViewBtn").click()
+                        poco("RewardPreView").offspring("Bg").child("Close").click()
+                        poco("SingleMatchBtn").click()
+                        poco("SingleMatchBtn").click()
+                        poco("TeamMatchBtn").click()
+                        poco("Close").click()
+                        printcolor.printgreen("英雄战场所有按钮点击关闭操作正常")
+                    except Exception as e:
+                        printcolor.printred("英雄战场界面元素点击报错")
+                        printcolor.printred(e)
+                        poco("Close").click()
+                        screenshot.get_screen_shot(time.time(), devices, "英雄战场界面元素点击报错")
                 poco("MobaActivityDlg(Clone)").offspring("item3").click()  # 进入全球战场
                 with poco.freeze() as freezepoco:
                     if freezepoco("0").exists() and \
@@ -273,20 +277,21 @@ def athletics(devices):
                         printcolor.printgreen("大标题，介绍内容，全球榜，排行榜，前往挑战按钮，可能获得的奖励显示正常")
                     else:
                         printcolor.printred("大标题，介绍内容，全球榜，排行榜，前往挑战按钮，可能获得的奖励等元素显示异常")
-                        # screenshot.get_screen_shot(time.time(), devices, "全球竞技缺少对应按钮")
-                        try:
-                            poco("0").child("Icon").click()
-                            poco("0").child("Icon").click()
-                            poco("RankBtn").click()
-                            poco(texture="l_close_00").click()
-                            poco("GoBtn").click()
-                            poco("Close").click()
-                            poco("Close").click()
-                            printcolor.printgreen("全球竞技缺所有按钮点击关闭操作正常")
-                        except Exception as e:
-                            printcolor.printred("全球竞技缺界面元素点击报错")
-                            printcolor.printred(e)
-                            # screenshot.get_screen_shot(time.time(), devices, "全球竞技缺界面元素点击报错")
+                        screenshot.get_screen_shot(time.time(), devices, "全球竞技缺少对应按钮")
+                    try:
+                        poco("0").child("Icon").click()
+                        poco("0").child("Icon").click()
+                        poco("RankBtn").click()
+                        poco("WeekNestRank").child("Close").click()
+                        poco("GoBtn").click()
+                        poco("Close").click()
+                        poco("Close").click()
+                        printcolor.printgreen("全球竞技缺所有按钮点击关闭操作正常")
+                    except Exception as e:
+                        printcolor.printred("全球竞技缺界面元素点击报错")
+                        printcolor.printred(e)
+                        poco("Close").click()
+                        screenshot.get_screen_shot(time.time(), devices, "全球竞技缺界面元素点击报错")
                 poco("MobaActivityDlg(Clone)").offspring("item4").click()  # 进入一个天梯赛
                 with poco.freeze() as freezepoco:
                     if freezepoco("CaptainDlg(Clone)").offspring("WeekReward").child("ListPanel").exists() and \
@@ -302,32 +307,33 @@ def athletics(devices):
                         printcolor.printgreen("周奖励，日奖励，单人匹配，组队匹配，荣誉商店，对战记录，规则，场次计算显示正常")
                     else:
                         printcolor.printred("周奖励，日奖励，单人匹配，组队匹配，荣誉商店，对战记录，规则，场次计算显示正常")
-                        # screenshot.get_screen_shot(time.time(), devices, "保护队长元素报错")
-                        try:
-                            poco("Help").click()
-                            poco("Btn").click()
-                            poco("BtnShop").click()
-                            poco("Close").click()
-                            poco("BtnRecord").click()
-                            poco(texture="l_close_00").click()
-                            poco("BtnStartSingle").click()
-                            poco("BtnStartSingle").click()
-                            poco("BtnStartTeam").click()
-                            poco("Close").click()
-                            printcolor.printgreen("保护队长所有按钮点击关闭操作正常")
-                        except Exception as e:
-                            printcolor.printred("保护队长界面元素点击报错")
-                            printcolor.printred(e)
-                            # screenshot.get_screen_shot(time.time(), devices, "保护队长报错")
+                        screenshot.get_screen_shot(time.time(), devices, "保护队长元素报错")
+                    try:
+                        poco("Help").click()
+                        poco("Btn").click()
+                        poco("BtnShop").click()
+                        poco("Close").click()
+                        poco("BtnRecord").click()
+                        poco(texture="l_close_00").click()
+                        poco("BtnStartSingle").click()
+                        poco("BtnStartSingle").click()
+                        poco("BtnStartTeam").click()
+                        poco("Close").click()
+                        printcolor.printgreen("保护队长所有按钮点击关闭操作正常")
+                    except Exception as e:
+                        printcolor.printred("保护队长界面元素点击报错")
+                        printcolor.printred(e)
+                        poco("Close").click()
+                        screenshot.get_screen_shot(time.time(), devices, "保护队长报错")
              else:
                 printcolor.printred("界面缺少元素，麻烦检查")
-                # screenshot.get_screen_shot(time.time(), devices, "竞技军衔按钮不存在")
+                screenshot.get_screen_shot(time.time(), devices, "竞技军衔按钮不存在")
          else:
              printcolor.printred("竞技名人堂按钮不存在")
-             # screenshot.get_screen_shot(time.time(), devices, "竞技名人堂按钮不存在")
+             screenshot.get_screen_shot(time.time(), devices, "竞技名人堂按钮不存在")
      else:
          printcolor.printred("竞技场暂未开放，请升级或者查看相关配置")
-         # screenshot.get_screen_shot(time.time(), devices, "竞技功能未开放")
+         screenshot.get_screen_shot(time.time(), devices, "竞技功能未开放")
      return poco("Btn_MobaRank").get_name()  # 返回值
 # devices = "127.0.0.1:62001"
 # athletics(devices)
