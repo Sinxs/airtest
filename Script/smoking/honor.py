f17066b28a6ab24cdcda7347bf9ba657c5fe979f
sinwu <157366763@QQ.com>
2019-06-15 15:42:01 +0800

    ps：上传头衔模块自动化脚本（正式）

--- Script/smoking/honor.py
+++ Script/smoking/honor.py
@@ -1,10 +1,7 @@
 from multi_processframe.Tools import printcolor,adb_connect,screenshot
 from airtest.core.api import *
-from poco.drivers.unity3d import UnityPoco
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 Androidpoco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
-
-devices = "127.0.0.1:62025"
 
 def honor(devices):
     poco = adb_connect.device(devices)
@@ -26,10 +23,10 @@ def honor(devices):
                 printcolor.printgreen("检查点 " + freeze_poco("TitleDlg(Clone)").offspring("Current").offspring("FightLabel").get_text() + " 显示正确")
                 printcolor.printgreen("检查点 " + freeze_poco("TitleDlg(Clone)").offspring("Current").offspring("item0").child("Label").get_text() + " 显示正确")
                 printcolor.printgreen("检查点 " + freeze_poco("TitleDlg(Clone)").offspring("Current").offspring("item1").child("Label").get_text() + " 显示正确")
-                if poco("TitleDlg(Clone)").offspring("RedPoint").exists():  # 如果有可以晋升的头衔，就点击晋升，但是只点击一次
-                    printcolor.printgreen("有可以晋升的选项，点击晋升....")
-                    poco("Promote").click()  # 点击晋升
-                    sleep(6)
+                printcolor.printgreen("点击晋升....")
+                poco("Promote").click()  # 点击晋升
+                sleep(6)
+                if poco("KeepOn").exists():  # 如果有可以晋升的头衔，就点击晋升，但是只点击一次
                     freeze_poco = poco.freeze()  # TODO：定义dongjiepoco
                     printcolor.printgreen("进入头衔晋升界面")
                     printcolor.printgreen("检查点 " + freeze_poco("TitleShareDlg(Clone)").child("Bg").child("Title").get_text() + " 显示正确")
@@ -41,20 +38,105 @@ def honor(devices):
                         if freeze_poco("TitleDlg(Clone)").offspring("Title").exists():
                             printcolor.printgreen("进入头衔晋升界面")
                             printcolor.printgreen("检查点 " + freeze_poco("TitleDlg(Clone)").offspring("Title").get_text() + " 显示正确")
-                            printcolor.printgreen( "检查点 " + freeze_poco("TitleDlg(Clone)").offspring("Current").offspring("FightLabel"))
+                            printcolor.printgreen("检查点 " + freeze_poco("TitleDlg(Clone)").offspring("Current").offspring("FightLabel").get_text() + " 显示正确")
                             printcolor.printgreen("检查点 " + freeze_poco("TitleDlg(Clone)").offspring("Current").offspring("item0").child("Label").get_text() + " 显示正确")
-                            printcolor.printgreen( "检查点 " + freeze_poco("TitleDlg(Clone)").offspring("Current").offspring("item1").child("Label").get_text() + " 显示正确")
+                            printcolor.printgreen("检查点 " + freeze_poco("TitleDlg(Clone)").offspring("Current").offspring("item1").child("Label").get_text() + " 显示正确")
+                            poco(texture="l_close_00").click()
+                            poco("XSys_Bag_Item").click()  # 点击背包按钮
+                            printcolor.printgreen("点击返回到角色界面，并且点击背包按钮，下面开始测试勋章合成功能")
                         else:
                             printcolor.printred("没有返回晋升界面，请检查...")
                             screenshot.get_screen_shot(time.time(), devices, "没有返回晋升界面")
                 else:
                     printcolor.printgreen("没有可以晋升的选项，无法晋升")
-                # todo：接下来判断没有晋升选项的状态，如果没有晋升，就给出提示直接跳过，进入下一个状态的判断，剩下是的合成勋章的判断
-
-
+                    poco(texture="l_close_00").click()
+                    poco("XSys_Bag_Item").click()  # 点击背包按钮
+                    printcolor.printgreen("点击返回到角色界面，并且点击背包按钮，下面开始测试勋章合成功能")
+                if poco("ItemNewDlg(Clone)").offspring("item301").child("Num").exists():  # 判断背包中勋章的数量，如果数量不足以合成就跳过
+                    printcolor.printgreen("背包存在战斗勋章，开始检查勋章数量")
+                    if int(poco("ItemNewDlg(Clone)").offspring("item301").child("Num").get_text()) > 3:
+                        printcolor.printgreen("战斗勋章数量>3，开始进行合成操作...")
+                        poco("ItemNewDlg(Clone)").offspring("item301").click()
+                        freeze_poco = poco.freeze()  # TODO：定义冻结poco
+                        if freeze_poco("TopFrame").offspring("Name").exists():
+                            printcolor.printgreen("检查点 " + freeze_poco("Text0").offspring("T").get_text() + "     " + freeze_poco( "Text0").get_text() + "      显示正确")
+                            printcolor.printgreen("检查点 " + freeze_poco("Text1").offspring("T").get_text() + "     " + freeze_poco("Text1").get_text() + "      显示正确")
+                            printcolor.printgreen("检查点 " + freeze_poco("Text2").offspring("T").get_text() + "     " + freeze_poco("Text2").get_text() + "      显示正确")
+                            printcolor.printgreen("检查点 " + freeze_poco("Binding").offspring("T").get_text() + "     " + freeze_poco("Binding").offspring("Yes").get_text() + "        显示正确")
+                            printcolor.printgreen("检查点 " + freeze_poco("Description").offspring("Title").get_text() + "     " + freeze_poco("Text").get_text() + "        显示正确")
+                            if poco("Button1").exists():
+                                printcolor.printgreen("点击晋升按钮")
+                                poco("Button1").click()  # 点击晋升
+                                if poco("Title").exists():
+                                    printcolor.printgreen("进入晋升界面成功")
+                                    poco(texture="l_close_00").click()  # 点击返回
+                                    poco("ItemNewDlg(Clone)").offspring("item301").click()  # 再次点击勋章
+                                else:
+                                    printcolor.printred("晋升界面元素控件缺失，请检查...")
+                                    screenshot.get_screen_shot(time.time(), devices, "没有弹出晋升弹窗")
+                            else:
+                                printcolor.printred("没有弹出晋升弹窗，请检查...")
+                                screenshot.get_screen_shot(time.time(), devices, "没有弹出晋升弹窗")
+                            if poco("Button5").exists():  # 点击勋章升级按钮
+                                poco("Button5").click()
+                                if poco("GreyModalDlg(Clone)").child("Bg").child("Label").exists():
+                                    printcolor.printgreen("检查点 " + poco("GreyModalDlg(Clone)").child("Bg").child("Label") .get_text() + "显示正确")
+                                    poco("OK").click()  # 点击确定按钮，进行合成
+                                    printcolor.printgreen("合成成功")
+                            else:
+                                printcolor.printred("没有弹出合成弹窗，请检查...")
+                                screenshot.get_screen_shot(time.time(), devices, "没有弹出合成弹窗")
+                    else:
+                        printcolor.printgreen("战斗勋章不足，不做合成操作")
+                        screenshot.get_screen_shot(time.time(), devices, "勋章不足")
+                else:
+                    printcolor.printgreen("背包没有战斗勋章，不做合成操作")
+                    screenshot.get_screen_shot(time.time(), devices, "背包没有勋章")
+                if poco("ItemNewDlg(Clone)").offspring("item302").child("Num").exists():  # 判断背包中勋章的数量，如果数量不足以合成就跳过
+                    printcolor.printgreen("背包存在宫廷勋章，开始检查勋章数量")
+                    if int(poco("ItemNewDlg(Clone)").offspring("item302").child("Num").get_text()) > 3:
+                        printcolor.printgreen("宫廷勋章数量>3，开始进行合成操作...")
+                        poco("ItemNewDlg(Clone)").offspring("item302").click()
+                        freeze_poco = poco.freeze()  # TODO：定义冻结poco
+                        if freeze_poco("TopFrame").offspring("Name").exists():
+                            printcolor.printgreen("检查点 " + freeze_poco("Text0").offspring("T").get_text() + "     " + freeze_poco("Text0").get_text() + "      显示正确")
+                            printcolor.printgreen("检查点 " + freeze_poco("Text1").offspring("T").get_text() + "     " + freeze_poco("Text1").get_text() + "      显示正确")
+                            printcolor.printgreen("检查点 " + freeze_poco("Text2").offspring("T").get_text() + "     " + freeze_poco("Text2").get_text() + "      显示正确")
+                            printcolor.printgreen("检查点 " + freeze_poco("Binding").offspring("T").get_text() + "     " + freeze_poco("Binding").offspring("Yes").get_text() + "        显示正确")
+                            printcolor.printgreen("检查点 " + freeze_poco("Description").offspring("Title").get_text() + "     " + freeze_poco( "Text").get_text() + "        显示正确")
+                            if poco("Button1").exists():
+                                printcolor.printgreen("点击晋升按钮")
+                                poco("Button1").click()  # 点击晋升
+                                if poco("Title").exists():
+                                    printcolor.printgreen("进入晋升界面成功")
+                                    poco(texture="l_close_00").click()  # 点击返回
+                                    poco("ItemNewDlg(Clone)").offspring("item302").click()  # 再次点击勋章
+                                else:
+                                    printcolor.printred("晋升界面元素控件缺失，请检查...")
+                                    screenshot.get_screen_shot(time.time(), devices, "没有弹出晋升弹窗")
+                            else:
+                                printcolor.printred("没有弹出晋升弹窗，请检查...")
+                                screenshot.get_screen_shot(time.time(), devices, "没有弹出晋升弹窗")
+                            if poco("Button5").exists():  # 点击勋章升级按钮
+                                poco("Button5").click()
+                                if poco("GreyModalDlg(Clone)").child("Bg").child("Label").exists():
+                                    printcolor.printgreen("检查点 " + poco("GreyModalDlg(Clone)").child("Bg").child("Label") .get_text() + "显示正确")
+                                    poco("OK").click()  # 点击确定按钮，进行合成
+                                    printcolor.printgreen("合成成功")
+                            else:
+                                printcolor.printred("没有弹出合成弹窗，请检查...")
+                                screenshot.get_screen_shot(time.time(), devices, "没有弹出合成弹窗")
+                    else:
+                        printcolor.printgreen("宫廷勋章不足，不做合成操作")
+                        screenshot.get_screen_shot(time.time(), devices, "勋章不足")
+                else:
+                    printcolor.printgreen("背包没有勋章，不做合成操作")
+                    screenshot.get_screen_shot(time.time(), devices, "背包没有勋章")
         else:
             printcolor.printred("没有进入装备界面，请检查...")
             screenshot.get_screen_shot(time.time(), devices, "主界面缺少日常按钮")
     else:
         printcolor.printred("没有进入装备界面，请检查...")
         screenshot.get_screen_shot(time.time(), devices, "主界面缺少日常按钮")
+    return poco("ItemNewDlg(Clone)").offspring("Frame").offspring("T").get_text()
+
---
+++ multi_processframe/TestCase/TC_honor.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+__author__ = "sinwu"
+
+import unittest
+from airtest.core.api import *
+from Script.smoking import honor
+from multi_processframe.Tools import initial, screenshot
+from poco.drivers.unity3d import UnityPoco
+poco = UnityPoco()
+
+def Main(devices):
+    class TC_honor(unittest.TestCase):
+        u'''测试用例102的集合'''
+
+        @classmethod
+        def setUpClass(self):
+            u''' 这里放需要在所有用例执行前执行的部分'''
+            pass
+
+        def setUp(self):
+            u'''这里放需要在每条用例前执行的部分'''
+            initial.startgame(devices)
+
+        def test_home(self):
+            """
+            头衔模块-头衔晋升and界面判断-勋章合成and界面判断
+            """
+            try:
+                print("开始测试头衔模块")
+                self.assertEqual("背 包", honor.honor(devices))
+            finally:
+                screenshot.get_screen_shot(time.time(), devices, "头衔-冒烟测试")
+
+
+
+        def tearDown(self):
+            u'''这里放需要在每条用例后执行的部分'''
+            print(f"{devices}结束运行")
+
+        @classmethod
+        def tearDownClass(self):
+            u'''这里放需要在所有用例后执行的部分'''
+            pass
+
+    srcSuite = unittest.makeSuite(TC_honor)
+    return srcSuite
