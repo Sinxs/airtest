b278ff559cb1814ce61d58414cba8de8336a39ec
xiaomingli <xiaomingli@123u.com>
2019-06-17 16:27:09 +0800

    ps：提交龙器模块，修改TC龙玉模块

--- multi_processframe/TestCase/TC_longqi.py
+++ multi_processframe/TestCase/TC_longqi.py
@@ -3,12 +3,12 @@ __author__ = "Lee.li"
 
 import unittest
 from airtest.core.api import *
-from Script.smoking import longyu
+from Script.smoking import longqi
 from multi_processframe.Tools import initial, screenshot
 
 
 def Main(devices):
-    class TC_longyu(unittest.TestCase):
+    class TClongqi(unittest.TestCase):
         u'''测试用例102的集合'''
 
         @classmethod
@@ -22,12 +22,12 @@ def Main(devices):
 
         def test_longyu(self):
             """
-            龙玉功能测试模块--主要检测龙玉界面按钮，龙玉购买，龙玉镶嵌等功能
+            龙器功能测试模块--主要检测龙器界面按钮，龙器穿戴，龙器获取，龙器神炉个按钮点击操作
             """
             try:
-                self.assertEqual("Duck", longyu.item_jade(devices))
+                self.assertEqual("Duck", longqi.Artifact(devices))
             finally:
-                screenshot.get_screen_shot(time.time(), devices, "龙玉功能测试模块")
+                screenshot.get_screen_shot(time.time(), devices, "龙器功能测试模块")
 
         def tearDown(self):
             u'''这里放需要在每条用例后执行的部分'''
@@ -38,5 +38,5 @@ def Main(devices):
             u'''这里放需要在所有用例后执行的部分'''
             pass
 
-    srcSuite = unittest.makeSuite(TC_longyu)
+    srcSuite = unittest.makeSuite(TClongqi)
     return srcSuite
