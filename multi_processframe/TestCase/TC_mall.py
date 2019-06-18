e6bc280cbba2a8b440f48a2404df2517abe8340e
sinwu <157366763@QQ.com>
2019-06-18 15:21:26 +0800

    ps：修改商店模块代码，对比结果修正

--- multi_processframe/TestCase/TC_mall.py
+++ multi_processframe/TestCase/TC_mall.py
@@ -26,7 +26,7 @@ def Main(devices):
             """
             try:
                 print("开始测试商店模块")
-                self.assertEqual("商 店", gamemall.mall(devices))
+                self.assertEqual("战队商店", gamemall.mall(devices))
             finally:
                 screenshot.get_screen_shot(time.time(), devices, "商店-冒烟测试")
 
