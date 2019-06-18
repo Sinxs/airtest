25204f975ab3802e9f553a238fc1cee4166cc6e4
xiaomingli <xiaomingli@123u.com>
2019-06-18 11:28:55 +0800

    ps:提交优化坐骑模块

--- Script/smoking/horse.py
+++ Script/smoking/horse.py
@@ -17,26 +17,35 @@ def horse(devices):
     with poco.freeze() as freeze_poco:
         freeze_poco("NewBtn").child("BtnBg").click() # 打开坐骑列表
         petgrid = freeze_poco("Grid").child() # 获取目前所有坐骑数量
-        swipe((486, 170), (486, 889), 4)
-        swipe((486, 170), (486, 889), 4)
-        swipe((486, 170), (486, 889), 4)
+        for x in range(20):
+            if poco("Grid").child("item0").get_position()[1] < 0.145:
+                swipe((486, 170), (486, 889))
+            else:
+                break
     for i in range(len(petgrid)):
         item = "item" + str(i)
-        if i == 25:
+        if i >= 25:
             for i in range(20):
                 if poco("Grid").child(item).get_position()[1] > 0.9:
                     swipe((200, 560), (200, 180))
                 else:
                     break
-        poco("Grid").offspring(item).click()  # 点击坐骑icon
+        with poco.freeze() as pocoone:
+            pocoone("Grid").offspring(item).click()  # 点击坐骑icon
         with poco.freeze() as freezepoco:
             if freezepoco("Btnhave").exists() or freezepoco("BtnMount").exists(): #判断坐骑打开是否正常是否存在
                 petname = freezepoco("PetName").offspring("Name").get_text() # 获得当前坐骑的名称
                 printcolor.printgreen(f"{petname}坐骑查看正常",end=",")
+                if freezepoco("Vehicle_horse").exists():
+                    printcolor.printgreen(f"坐骑显示模型", end=",")
+                else:
+                    printcolor.printred("坐骑模型不显示，请详细查看", end=",")
+                    screenshot.get_screen_shot(time.time(), devices, "坐骑模型不显示")
                 if freezepoco("Btnhave").exists():
                     freezepoco("Btnhave").click()
-                    name = poco("ItemToolTipDlg(Clone)").child("Bg").offspring("TopFrame").child("Name").get_text()
-                    printcolor.printgreen(f"{name}获取界面打开正常")
+                    with poco.freeze() as pocotwo:
+                        name = pocotwo("ItemToolTipDlg(Clone)").child("Bg").offspring("TopFrame").child("Name").get_text()
+                    printcolor.printgreen("获取界面打开正常")
                     touch([1140,540],times=2)
                 else:
                     if freezepoco("BtnMount").exists():
@@ -45,7 +54,7 @@ def horse(devices):
                         printcolor.printred("没有相关坐骑信息，请查具体查看该坐骑！")
             else:
                 printcolor.printred("没有获取到该坐骑相关信息")
-                # screenshot.get_screen_shot(time.time(), devices, "没有拿到这个坐骑的模型")
+                screenshot.get_screen_shot(time.time(), devices, "没有获取到该坐骑相关信息")
             freezepoco("NewBtn").child("BtnBg").click() # 打开坐骑列表
             time.sleep(1)
     return poco("Btnhave").get_name() # 返回值
@@ -59,5 +68,5 @@ def check_menu(sysmenu, poco):
     else:
         poco(sysmenu).click()
 
-devices = "127.0.0.1:62001"
-horse(devices)
+# devices = "127.0.0.1:62001"
+# horse(devices)
