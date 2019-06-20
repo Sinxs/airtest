ae6af3f32af6726649b535683d92bed3b280bf73
sinwu <157366763@QQ.com>
2019-06-20 13:40:36 +0800

    ps：上传队伍模块

---
+++ multi_processframe/TestCase/TC_troops.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+__author__ = "sinwu"
+
+import unittest
+from airtest.core.api import *
+from Script.smoking import troops
+from multi_processframe.Tools import initial, screenshot
+from poco.drivers.unity3d import UnityPoco
+poco = UnityPoco()
+
+def Main(devices):
+    class TC_troops(unittest.TestCase):
+        u'''测试用例102的集合'''
+
+        @classmethod
+        def setUpClass(self):
+            u''' 这里放需要在所有用例执行前执行的部分'''
+            pass
+
+        def setUp(self):
+            u'''这里放需要在每条用例前执行的部分'''
+            initial.startgame(devices)
+
+        def test_task(self):
+            """
+            队伍模块 - 队伍的各个界面控件判断，按钮点击，功能操作 ps：单人操作，没有交互
+            """
+            try:
+                print("开始测试日常队伍模块")
+                self.assertEqual("队友验证", troops.troops(devices))
+            finally:
+                screenshot.get_screen_shot(time.time(), devices, "队伍模块-冒烟测试")
+
+
+        def tearDown(self):
+            u'''这里放需要在每条用例后执行的部分'''
+            print(f"{devices}结束运行")
+
+        @classmethod
+        def tearDownClass(self):
+            u'''这里放需要在所有用例后执行的部分'''
+            pass
+
+    srcSuite = unittest.makeSuite(TC_troops)
+    return srcSuite
