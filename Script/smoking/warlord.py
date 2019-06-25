fd0a41e15f7a1fd20c1be33708f82b149b6acd01
xiaomingli <xiaomingli@123u.com>
2019-06-25 14:38:14 +0800

    ps：上传佣兵模块

--- Script/smoking/warlord.py
+++ Script/smoking/warlord.py
@@ -11,36 +11,46 @@ def npcfavor(devices):
     """
     poco = adb_connect.device(devices)
     check_menu("SysF_Machine", poco)  # 进入精灵
-    if poco("BookBg").exists():  # 判断伙伴界面是否存在
+    if poco("MachineArmorDlg(Clone)").exists():  # 判断伙伴界面是否存在
         with poco.freeze() as freeze_poco:
-            if freeze_poco("item0").child("selected").exists() and \
-                    freeze_poco("item1").child("selected").exists() and \
-                    freeze_poco("item2").child("selected").exists() and \
-                    freeze_poco("StarCount").exists() and \
-                    freeze_poco("Stars").exists() and \
-                    freeze_poco("BtnReward").exists() and \
-                    freeze_poco("BtnAttrTotal").exists() and \
-                    freeze_poco("WrapContent").exists() and \
-                    freeze_poco("MonsterpreferenceDlg(Clone)").child("Help").exists() and \
-                    freeze_poco("Icons").offspring("item0").exists() and \
-                    freeze_poco("Icons").offspring("item1").exists() and \
-                    freeze_poco("Select").exists() and \
-                    freeze_poco("ActiveBtn").exists():
-                printcolor.printgreen("伙伴界面UI元素显示正常")
+            if freeze_poco("MachineArmorDlg(Clone)").offspring("MachineScrollview").child("Wrapcontent").exists() and \
+                    freeze_poco("Title1").exists() and \
+                    freeze_poco("Title2").exists() and \
+                    freeze_poco("ActivationBtn").exists() and \
+                    freeze_poco("List").exists() and \
+                    freeze_poco("say").exists() and \
+                    freeze_poco("SnapDrag").exists() and \
+                    freeze_poco("MachineArmorDlg(Clone)").offspring("SkillScrollview").child("Wrapcontent").exists():
+                printcolor.printgreen("佣兵界面UI元素显示正常")
             else:
-                printcolor.printred("伙伴界面UI元素显示异常，详情见截图")
+                printcolor.printred("佣兵界面UI元素显示异常，详情见截图")
                 screenshot.get_screen_shot(time.time(), devices, "伙伴界面UI元素显示异常")
             try:
-                freeze_poco("item1").child("selected").click()
-                freeze_poco("item2").child("selected").click()
-                freeze_poco("MonsterpreferenceDlg(Clone)").child("Help").click()
-                poco("Btn").click()
-                freeze_poco("BtnReward").click()
-                poco("PointRewardFrame").offspring("Close").click()
-                freeze_poco("BtnAttrTotal").click()
-                freeze_poco("BtnAttrTotal").click()
-                freeze_poco("ActiveBtn").click()
-                printcolor.printgreen("伙伴界面按钮点击正常")
+                with poco.freeze() as freeze_poco:
+                    for i in freeze_poco("MachineArmorDlg(Clone)").offspring("MachineScrollview").child("Wrapcontent").offspring("HeadIcon"):  # 循环有多少个佣兵
+                        uiname = i.parent().get_name()
+                        name = freeze_poco(uiname).child("Name").get_text()
+                        freeze_poco(uiname).click()
+                        poco("MachineArmorDlg(Clone)").offspring("MachineScrollview").offspring(uiname).offspring("Frame").click()
+                        poco("Obtain").click()
+                        poco("Obtain").click()
+                        if poco("main").exists():
+                            poco("Obtain").click()
+                        poco(texture="l_close_00").click()
+                        if poco("SnapDrag").exists():
+
+                            printcolor.printgreen(f"{name}佣兵模型显示正常")
+                        else:
+                            printcolor.printred(f"{name}佣兵模型不显示")
+                            screenshot.get_screen_shot(time.time(), devices, "佣兵模型不显示")
+                        freeze_poco("ActivationBtn").click()
+                        freeze_poco("ActivationBtn").click()
+                        with poco.freeze() as freeze_poco1:
+                            for i in freeze_poco1("MachineArmorDlg(Clone)").offspring("SkillScrollview").child("Wrapcontent").child():  # 循环有多少个技能
+                                i.click()
+                                if poco("MachineSkillPreView(Clone)").child("Bg").child("p").exists():
+                                    i.click()
+                    printcolor.printgreen("佣兵模块所有按钮点击正常")
             except Exception as e:
                 printcolor.printred("伙伴界面按钮点击流程异常")
                 printcolor.printred(e)
@@ -61,5 +71,5 @@ def check_menu(sysmenu, poco):
         poco(sysmenu).click()
 
 
-devices = "127.0.0.1:62001"
-npcfavor(devices)
+# devices = "127.0.0.1:62001"
+# npcfavor(devices)
