458933f9c8d7bbc2dd3889cae7fa6b7447a8f042
Sinwu <157366763@QQ.com>
2019-11-06 10:02:42 +0800

    删除初始文件

--- multi_processframe/TestCase/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = "Lee.li"
-import os
-from multi_processframe.Tools import analysis
-
-case_Path = os.path.join(os.getcwd(), "TestCase")
-script_List = analysis.get_script_list(case_Path)
-__all__ = script_List
--- multi_processframe/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding=utf8 -*-
-#构造方法
-
-__all__ = ["Tools","TestCase"]
-print(__all__)
